a6aca7bea241697d0fba07f0c9a279f39915dbec
Yaramir Bolko <yaramir.bolko@yandex.ru>
2020-12-10 21:16:59 +0300

    add GraphicsDisplay class

--- src/lab_4/GraphicsDisplay.java
+++ src/lab_4/GraphicsDisplay.java
@@ -3,4 +3,131 @@ package lab_4;
 
 public class GraphicsDisplay extends JPanel {
 
+
+    // Список координат точек для построения графика
+    private Double[][] graphicsData;
+    // Флаговые переменные, задающие правила отображения графика
+    private boolean showAxis = true;
+    private boolean showMarkers = true;
+    // Границы диапазона пространства, подлежащего отображению
+    private double minX;
+    private double maxX;
+    private double minY;
+    private double maxY;
+    // Используемый масштаб отображения
+    private double scale;
+    // Различные стили черчения линий
+    private BasicStroke graphicsStroke;
+    private BasicStroke axisStroke;
+    private BasicStroke markerStroke;
+    // Шрифт отображения подписей к осям координат
+    private Font axisFont;
+
+    //инициализация повторно используемых объектов, представляющих стили черчения линий и шрифт
+    public GraphicsDisplay() {
+        // Цвет заднего фона области отображения - белый
+        setBackground(Color.WHITE);
+        // Сконструировать необходимые объекты, используемые в рисовании
+        // Перо для рисования графика
+        graphicsStroke = new BasicStroke(3.0f, BasicStroke.CAP_BUTT,
+            BasicStroke.JOIN_ROUND, 10.0f, new float[]{80, 20, 20, 20, 20, 20, 40, 20,40}, 0.0f);
+        // Перо для рисования осей координат
+        axisStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT,
+            BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+        // Перо для рисования контуров маркеров
+        markerStroke = new BasicStroke(11.0f, BasicStroke.CAP_BUTT,
+            BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+        // Шрифт для подписей осей координат
+        axisFont = new Font("Serif", Font.BOLD, 36);
+    }
+
+    // Метод вызывается из обработчика элемента меню "Открыть файл с графиком"
+    // главного окна приложения в случае успешной загрузки данных
+    public void showGraphics(Double[][] graphicsData) {
+        // Сохранить массив точек во внутреннем поле класса
+        this.graphicsData = graphicsData;
+        // Запросить перерисовку компонента (неявно вызвать paintComponent())
+        repaint();
+    }
+
+        //Метод отображения осей координат
+    protected void paintAxis(Graphics2D canvas) {
+        // Шаг 1 – установить необходимые настройки рисования
+        // Установить особое начертание для осей
+        canvas.setStroke(axisStroke);
+        // Оси рисуются чѐрным цветом
+        canvas.setColor(Color.BLACK);
+        // Стрелки заливаются чѐрным цветом
+        canvas.setPaint(Color.BLACK);
+        // Подписи к координатным осям делаются специальным шрифтом
+        canvas.setFont(axisFont);
+        // Создать объект контекста отображения текста - для получения
+        // характеристик устройства (экрана)
+        FontRenderContext context = canvas.getFontRenderContext();
+        // Шаг 2 - Определить, должна ли быть видна ось Y на графике
+        if (minX<=0.0 && maxX>=0.0) {
+            // Она видна, если левая граница показываемой области minX<=0.0,
+            // а правая (maxX) >= 0.0
+            // Шаг 2а - ось Y - это линия между точками (0, maxY) и (0, minY)
+            canvas.draw(new Line2D.Double(xyToPoint(0, maxY), xyToPoint(0,minY)));
+
+            // Шаг 2б - Стрелка оси Y
+            GeneralPath arrow = new GeneralPath();
+            // Установить начальную точку ломаной точно на верхний конец оси Y
+            Point2D.Double lineEnd = xyToPoint(0, maxY);
+            arrow.moveTo(lineEnd.getX(), lineEnd.getY());
+            // Вести левый "скат" стрелки в точку с относительными
+            // координатами (5,20)
+            arrow.lineTo(arrow.getCurrentPoint().getX()+5,
+                arrow.getCurrentPoint().getY()+20);
+
+            // Вести нижнюю часть стрелки в точку с относительными
+            // координатами (-10, 0)
+            arrow.lineTo(arrow.getCurrentPoint().getX()-10,
+                arrow.getCurrentPoint().getY());
+
+            // Замкнуть треугольник стрелки
+            arrow.closePath();
+            canvas.draw(arrow); // Нарисовать стрелку
+            canvas.fill(arrow); // Закрасить стрелку
+            // Шаг 2в - Нарисовать подпись к оси Y
+            // Определить, сколько места понадобится для надписи “y”
+            Rectangle2D bounds = axisFont.getStringBounds("y", context);
+            Point2D.Double labelPos = xyToPoint(0, maxY);
+            // Вывести надпись в точке с вычисленными координатами
+            canvas.drawString("y", (float)labelPos.getX() + 10,(float)(labelPos.getY() - bounds.getY()));
+        }
+        // Шаг 3 - Определить, должна ли быть видна ось X на графике
+        if (minY<=0.0 && maxY>=0.0) {
+            // Она видна, если верхняя граница показываемой области max)>=0.0,
+            // а нижняя (minY) <= 0.0
+            // Шаг 3а - ось X - это линия между точками (minX, 0) и (maxX, 0)
+            canvas.draw(new Line2D.Double(xyToPoint(minX, 2), xyToPoint(maxX, 2)));
+
+            // Шаг 3б - Стрелка оси X
+            GeneralPath arrow = new GeneralPath();
+            // Установить начальную точку ломаной точно на правый конец оси X
+            Point2D.Double lineEnd = xyToPoint(maxX, 2);
+            arrow.moveTo(lineEnd.getX(), lineEnd.getY());
+            // Вести верхний "скат" стрелки в точку с относительными
+            // координатами (-20,-5)
+            arrow.lineTo(arrow.getCurrentPoint().getX()-20, arrow.getCurrentPoint().getY()-5);
+
+            // Вести левую часть стрелки в точку
+            // с относительными координатами (0, 10)
+            arrow.lineTo(arrow.getCurrentPoint().getX(), arrow.getCurrentPoint().getY()+10);
+
+            // Замкнуть треугольник стрелки
+            arrow.closePath();
+            canvas.draw(arrow); // Нарисовать стрелку
+            canvas.fill(arrow); // Закрасить стрелку
+            // Шаг 3в - Нарисовать подпись к оси X
+            // Определить, сколько места понадобится для надписи “x”
+            Rectangle2D bounds = axisFont.getStringBounds("x", context);
+            Point2D.Double labelPos = xyToPoint(maxX, 0);
+            // Вывести надпись в точке с вычисленными координатами
+            canvas.drawString("x",(float)(labelPos.getX()-bounds.getWidth()-20),
+                (float)(labelPos.getY() + bounds.getY()-200));
+        }
+    }
 }
